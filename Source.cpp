9e624eff59e8e8181a14f85e7139d958867b55d8
Roman <recska.roman@gmail.com>
2020-10-20 22:35:33 +0200

    JSON

--- Source.cpp
+++ Source.cpp
@@ -3,15 +3,12 @@ using namespace std;
 
 void Attack(Monster& monster1, Monster& monster2)
 {
-    std::cout << monster2.GetName() << " -> " << monster1.GetName() << std::endl;
     monster1.SetHp(monster1.GetHp() - monster2.GetDmg());
 }
 
 void Battle(Monster& monster1, Monster& monster2)
 {
     int n = 0;
-    monster1.ToString();
-    monster2.ToString();
     while (monster1.GetHp() > 0 && monster2.GetHp() > 0)
     {
         if ((n % 2 == 0) && monster1.GetHp() > 0)
@@ -19,12 +16,11 @@ void Battle(Monster& monster1, Monster& monster2)
         else if (monster2.GetHp() > 0)
             Attack(monster2, monster1);
 
-        monster1.ToString();
-        monster2.ToString();
+
         n++;
     }
-    if (monster1.GetHp() == 0) { cout << monster1.GetName() << " DIED! " << monster2.GetName() << " wins." << endl; }
-    else { cout << monster2.GetName() << " DIED! " << monster1.GetName() << " wins." << endl; }
+    if (monster1.GetHp() == 0) {std::cout << monster2.GetName() << " wins Remaining HP: "<< monster2.GetHp() << endl; }
+    else {std::cout << monster1.GetName() << " wins Remaining HP: " << monster1.GetHp() << endl; }
 }
 
 
