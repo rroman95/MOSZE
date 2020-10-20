66b15b8a0faecbf6fbb23b699444949587382b0f
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-10-21 01:23:47 +0200

    Create battle.sh

---
+++ battle.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+IFS=$'\n'
+
+rm output.txt
+
+./project units/thor.json units/loki.json >> output.txt
+./project units/thor.json units/hulk.json >> output.txt
+./project units/loki.json units/thor.json >> output.txt
+./project units/loki.json units/hulk.json >> output.txt 
+./project units/hulk.json units/thor.json >> output.txt
+./project units/hulk.json units/loki.json >> output.txt
+
+cat output.txt
+DIFF=$(diff output.txt good_output.txt) 
+
+if [ "$DIFF" != "" ] 
+then
+	echo "Not matching"
+	exit 1
+else
+	echo "Matching"
+fi
