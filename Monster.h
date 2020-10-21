fd40a229d2a99179de20634759e099f34555b347
Roman <recska.roman@gmail.com>
2020-10-21 15:47:57 +0200

    doxygen test

--- Monster.h
+++ Monster.h
@@ -17,9 +17,9 @@ public:
     Monster(string name, int hp, int dmg) : name(name), hp(hp), dmg(dmg),attackspeed(0),cooldown(0) {};
     Monster(string name, int hp, int dmg,double attackspeed) : name(name), hp(hp), dmg(dmg),attackspeed(attackspeed), cooldown(attackspeed) {};
     std::string GetName() const { return name; }
-    int GetHp() const { return hp; }
-    void SetHp(int);
-    int GetDmg() const { return dmg; }
+    int GetHp() const { return hp; }  					 ///< Lekéri a szörny életét
+    void SetHp(int);							 ///< Beállítja a szörny életét
+    int GetDmg() const { return dmg; }					 ///< Lekéri a szörny sebzését
     double GetAttackSpeed() { return attackspeed; }
     void ToString();
     static void Battle(Monster&, Monster&);
