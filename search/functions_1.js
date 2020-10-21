f7da983feba5eedb336114ececf1552db7bc499a
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-10-21 13:48:51 +0000

    Update gh-pages to output generated at fd40a22

---
+++ search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['sethp',['SetHp',['../classMonster.html#a28239e31d7e9d104a69ef5d06a7c98d3',1,'Monster']]]
+];
--- search/searchdata.js
+++ search/searchdata.js
@@ -1,21 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "m",
+  0: "gms",
   1: "m",
-  2: "m"
+  2: "gs",
+  3: "m"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "pages"
+  2: "functions",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Pages"
+  2: "Functions",
+  3: "Pages"
 };
 
