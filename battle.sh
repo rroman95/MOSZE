3a4695fcadf973ab262e64bb20211c19acc82fc1
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-10-21 13:44:18 +0200

    Update battle.sh

--- battle.sh
+++ battle.sh
@@ -10,14 +10,3 @@ rm output.txt
 ./project units/loki.json units/hulk.json >> output.txt 
 ./project units/hulk.json units/thor.json >> output.txt
 ./project units/hulk.json units/loki.json >> output.txt
-
-cat output.txt
-DIFF=$(diff output.txt good_output.txt) 
-
-if [ "$DIFF" != "" ] 
-then
-	echo "Not matching"
-	exit 1
-else
-	echo "Matching"
-fi
