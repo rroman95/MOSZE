d8fa2f9303c1793e1da8fd0d68c804b195099b3e
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-11-04 16:34:25 +0100

    testing for memory leak

--- Source.cpp
+++ Source.cpp
@@ -13,6 +13,7 @@ int main(int argc, char** argv)
         Monster monster1 = Monster::parseUnit(json1);
         Monster monster2 = Monster::parseUnit(json2);
         Monster::Battle(monster1, monster2);
+        new int = 10;
 
     }
     catch (int e)
