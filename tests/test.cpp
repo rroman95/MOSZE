36e88a52d195da74c4418872cb69e793df6038fd
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-11-10 15:14:09 +0100

    eccso

--- tests/test.cpp
+++ tests/test.cpp
@@ -6,7 +6,7 @@ TEST(Jsontest, Fajlvaltozo) {
 	std::ifstream f("Hosarkany.json");
 	JSON json1(f);
 	EXPECT_EQ(json1.charData["name"], "Hosarkany");
-	EXPECT_EQ(json1.charData["damage"], "30");
-	EXPECT_EQ(json1.charData["healthpoints"], "300");
+	EXPECT_EQ(json1.charData["damage"], "300");
+	EXPECT_EQ(json1.charData["healthpoints"], "30");
 
 }
