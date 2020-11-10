6dc101b34a3579d5f42c57502ae351b7da10d6fe
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-11-10 14:44:23 +0100

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,9 +4,7 @@
 
 TEST(Jsontest, Fajlvaltozo) {
 	std::ifstream f("Hosarkany.json");
-	JSON beolvasas = JSON::parseFromFile(f);
-	EXPECT_EQ(beolvasas.get<std::string>("name"), "Hosarkany");
-	EXPECT_EQ(beolvasas.get<int>("health_points"), 300);
-	EXPECT_EQ(beolvasas.get<int>("damage"), 30);
-	EXPECT_EQ(beolvasas.get<double>("attack_cooldown"), 2.4);
+	JSON::jsonparser(f);
+	EXPECT_EQ(charData["name"], "Hosarkany");
+
 }
