4e57104a74a485c4557102b307a02861f0420a54
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-11-10 14:58:27 +0100

    zsiványság

--- tests/test.cpp
+++ tests/test.cpp
@@ -6,5 +6,7 @@ TEST(Jsontest, Fajlvaltozo) {
 	std::ifstream f("Hosarkany.json");
 	JSON json1(f);
 	EXPECT_EQ(json1.charData["name"], "Hosarkany");
+	EXPECT_EQ(json1.charData["damage"], "40");
+	EXPECT_EQ(json1.charData["attack_cooldown"], "2.4");
 
 }
