3e545ee6b117e2133fabf48b0d866f836461e81f
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-11-10 15:27:55 +0100

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,11 +2,31 @@
 #include "gtest/gtest.h"
 
 
-TEST(Jsontest, Fajlvaltozo) {
+TEST(Jsontest, ifstream) {
 	std::ifstream f("Hosarkany.json");
+	JSON json1(f);
+	EXPECT_EQ(json1.charData["name"], "Hosarkany");
+	EXPECT_EQ(json1.charData["damage"], "30");
+	EXPECT_EQ(json1.charData["healthpoints"], "300");
+
+}
+
+TEST(Jsontest, Fajlvaltozo) {
+	std::string f = "Hosarkany.json";
 	JSON json1(f);
 	EXPECT_EQ(json1.charData["name"], "Hosarkany");
 	EXPECT_EQ(json1.charData["damage"], "300");
 	EXPECT_EQ(json1.charData["healthpoints"], "30");
 
 }
+
+TEST(Jsontest, Fajlvaltozo) {
+
+	JSON json1("thor.json");
+	EXPECT_EQ(json1.charData["name"], "thor");
+	EXPECT_EQ(json1.charData["damage"], "15.3");
+	EXPECT_EQ(json1.charData["hp"], "300");
+	EXPECT_EQ(json1.charData["attackcooldown"], "2");
+
+}
+
