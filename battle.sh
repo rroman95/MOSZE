40775d1b0922e44f78728f68215ab171bbff0119
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-11-11 15:25:17 +0100

    Update battle.sh

--- battle.sh
+++ battle.sh
@@ -4,3 +4,8 @@ IFS=$'\n'
 
 
 ./project units/thor.json units/loki.json >> output.txt
+./project units/thor.json units/hulk.json >> output.txt
+./project units/loki.json units/thor.json >> output.txt
+./project units/loki.json units/hulk.json >> output.txt 
+./project units/hulk.json units/thor.json >> output.txt
+./project units/hulk.json units/loki.json >> output.txt
