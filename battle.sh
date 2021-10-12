d71cdb69f8255450f029d31cd448ff98df6b425f
rroman95 <44027537+rroman95@users.noreply.github.com>
2021-10-12 21:31:59 +0200

    Update battle.sh

--- battle.sh
+++ battle.sh
@@ -9,3 +9,4 @@ IFS=$'\n'
 ./project units/loki.json units/hulk.json >> output.txt 
 ./project units/hulk.json units/thor.json >> output.txt
 ./project units/hulk.json units/loki.json >> output.txt
+"more" >> output.txt
